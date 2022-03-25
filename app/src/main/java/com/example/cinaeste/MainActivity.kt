6c75fb129c8a79a4b3beb733c18686345a37bb1e
acato2 <acato1@etf.unsa.ba>
2022-03-25 19:48:34 +0100

    Cinaeste - Zadatak 2

--- app/src/main/java/com/example/cinaeste/MainActivity.kt
+++ app/src/main/java/com/example/cinaeste/MainActivity.kt
@@ -1,40 +1,13 @@
 package com.example.cinaeste
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var listView: ListView
-    private lateinit var button: Button
-    private lateinit var editText: EditText
-    private val listaVrijednosti= arrayListOf<String>()
-    private lateinit var adapter: ArrayAdapter<String>
-
-    override fun onCreate(savedInstance : Bundle?){
-        super.onCreate(savedInstance)
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Inicijaliziramo elemente
-        button = findViewById<Button>(R.id.button1)
-        editText = findViewById<EditText>(R.id.editText1)
-        listView = findViewById<ListView>(R.id.listView1)
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,listaVrijednosti)
-        listView.adapter=adapter
-
-        button.setOnClickListener{
-            addToList()
-        }
-
-
     }
-
-    private fun addToList() {
-        listaVrijednosti.add(0,editText.text.toString())
-        adapter.notifyDataSetChanged();
-        editText.setText("")
-
-    }
-
 
 }
